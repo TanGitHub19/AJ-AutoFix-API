209319cf7a67d5e5f13c1178d0da3b88eaf63ba0
MarkTristanRaroque <145894944+TanGitHub19@users.noreply.github.com>
2024-09-11 19:55:25 +0800

    file uploads

--- config/file_upload.js
+++ config/file_upload.js
@@ -1,31 +1,36 @@
-const multer = require('multer');
-const path = require('path');
+const multer = require("multer");
+const path = require("path");
 
 const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, 'uploads/');
-    },
-    filename: (req, file, cb) => {
-        const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
-        cb(null, file.fieldname + '-' + uniqueSuffix + path.extname(file.originalname));
-    }
+  destination: (req, file, cb) => {
+    cb(null, "uploads/");
+  },
+  filename: (req, file, cb) => {
+    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+    cb(
+      null,
+      file.fieldname + "-" + uniqueSuffix + path.extname(file.originalname)
+    );
+  },
 });
 
 const fileFilter = (req, file, cb) => {
-    const filetypes = /jpeg|jpg|png|gif/;
-    const mimetype = filetypes.test(file.mimetype);
-    const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
+  console.log("File MIME type:", file.mimetype); // Log MIME type for debugging
 
-    if (mimetype && extname) {
-        return cb(null, true);
-    }
-    cb(new Error('Error: Images only!'));
+  const filetypes = /jpeg|jpg|png|gif/;
+  const mimetype = filetypes.test(file.mimetype);
+  const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
+
+  if (mimetype && extname) {
+    return cb(null, true);
+  }
+  cb(new Error("Error: Images only!"));
 };
 
 const upload = multer({
-    storage: storage,
-    limits: { fileSize: 1024 * 1024 * 5 }, 
-    fileFilter: fileFilter
+  storage: storage,
+  limits: { fileSize: 1024 * 1024 * 5 },
+  fileFilter: fileFilter,
 });
 
 module.exports = upload;
