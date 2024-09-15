f4d4ee1a78dbcb566215a9c2575c2c9507350d35
MarkTristanRaroque <145894944+TanGitHub19@users.noreply.github.com>
2024-09-15 17:18:36 +0800

    add

--- config/firebase.js
+++ config/firebase.js
@@ -1,9 +1,17 @@
-var admin = require("firebase-admin");
-var serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_JSON);
-admin.initializeApp({
-  credential: admin.credential.cert(JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_JSON)),
-  storageBucket: "gs://autofix-3cbff.appspot.com",
-});
+const admin = require("firebase-admin");
+
+try {
+  const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_JSON);
+
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    storageBucket: "gs://autofix-3cbff.appspot.com/profile_pictures",
+  });
+
+  console.log("Firebase Admin SDK initialized successfully");
+} catch (error) {
+  console.error("Error initializing Firebase Admin SDK:", error.message);
+}
 
 const bucket = admin.storage().bucket();
 
