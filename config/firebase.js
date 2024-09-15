a885a1b256594a9767d9bb594db3883175a8a2db
MarkTristanRaroque <145894944+TanGitHub19@users.noreply.github.com>
2024-09-15 17:11:46 +0800

    add

--- config/firebase.js
+++ config/firebase.js
@@ -1,7 +1,7 @@
 var admin = require("firebase-admin");
 var serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_JSON);
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+  credential: admin.credential.cert(JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_JSON)),
   storageBucket: "gs://autofix-3cbff.appspot.com",
 });
 
