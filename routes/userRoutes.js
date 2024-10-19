a9d91f8b0e75fc240cfc400808e1f133dc90e237
MarkTristanRaroque <145894944+TanGitHub19@users.noreply.github.com>
2024-10-19 19:43:57 +0800

    Added a upload in userRoutes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const userRouter = express.Router();
 const auth = require("../middleware/auth");
 const requireRole = require("../middleware/role");
-const upload = require("../middleware/file_upload");
+const { upload, uploadFile } = require("../middleware/file_upload");
 const {
   userUpdate,
   getUsers,
@@ -16,7 +16,7 @@ userRouter.get("/", getUsers);
 userRouter.get("/:id", getUser);
 userRouter.get("/:id", getUser);
 userRouter.get("/user/getUser", auth, getUsersByAuth);
-userRouter.put("/update/:id", auth, requireRole('user'), userUpdate);
+userRouter.put('/update/:id', upload.single('profilePicture'), uploadFile, userUpdate);
 userRouter.put("/admin/:id", auth, requireRole('admin'), updateUser);
 userRouter.delete("/:id", deleteUser);
 module.exports = userRouter;
